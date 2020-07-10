32136d7444d6e057483138885adff7275ad5d69a
JZH2018 <15156581529@163.com>
2020-07-10 10:21:26 +0800

    第六次提交  新增内容 对上次未完成的mapper作修改

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ gmall-user-web层   8080
 
 gmall-manage-service  8071
 gmall-manage-web      8081
+
+#gmall-item-service 前台的商品详情服务  8072
+gmall-item-web 前台的商品详情展示      8082
--- gmall-manage-service/src/main/java/com/jiang/gmall/manage/mapper/PmsProductSaleAttrMapper.java
+++ gmall-manage-service/src/main/java/com/jiang/gmall/manage/mapper/PmsProductSaleAttrMapper.java
@@ -9,17 +9,7 @@ import tk.mybatis.mapper.common.Mapper;
 import java.util.List;
 
 public interface PmsProductSaleAttrMapper extends Mapper<PmsProductSaleAttr> {
-    @Select("SELECT\n" +
-            "\tsa.*,\n" +
-            "\tsav.*,\n" +
-            "IF\n" +
-            "\t( ssav.sku_id, 1, 0 ) AS isChecked \n" +
-            "FROM\n" +
-            "\tpms_product_sale_attr sa\n" +
-            "\tINNER JOIN pms_product_sale_attr_value sav ON sa.product_id = sav.product_id \n" +
-            "\tAND sa.sale_attr_id = sav.sale_attr_id \n" +
-            "\tAND sa.product_id = #{productId}\n" +
-            "\tLEFT JOIN pms_sku_sale_attr_value ssav ON sav.id = ssav.sale_attr_value_id \n" +
-            "\tAND ssav.sku_id = #{skuId}")
+
+
     List<PmsProductSaleAttr> selectSpuSaleAttrListCheckBySku(@Param("productId") String productId, @Param("skuId")String skuId);
 }
